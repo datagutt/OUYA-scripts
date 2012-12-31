3ad705c00750985591d868cb180f6e2a1a6b8e3b
datagutt <datagutt666@hotmail.com>
2012-12-31 16:11:22 +0100

    Add scripts for converting an android apk to an ouya game or app

---
+++ convertToOUYAapp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+clear;
+echo 'Enter name of apk you want to convert:';
+read file;
+apktool d -f $file;
+filename=$(basename $file)
+outFolder=${filename%.*};
+if [ -d $outFolder ]; then
+	echo 'Converting apk to OUYA app...';
+	xmlFile=$outFolder'/AndroidManifest.xml';
+	contents=`cat $xmlFile`;
+	newContents=`awk '/android.intent.category.LAUNCHER/{print "<category android:name=\"ouya.intent.category.APP\"/>"}1' $xmlFile`
+	rm $xmlFile;
+	touch $xmlFile;
+	echo $newContents >> $xmlFile;
+	echo 'Contents of AndroidManifest.xml replaced.';
+	echo 'Now building the new apk...';
+	apktool b $outFolder $file
+	echo 'Enter android as passphrase.';
+	jarsigner -verbose -keystore $HOME'/.android/debug.keystore' $file androiddebugkey
+fi
